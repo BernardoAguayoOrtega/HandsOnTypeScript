0fb7f70f93015b457dce58ecc8503f3feeea0771
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2023-02-01 19:51:42 -0600

    🚧 any and shape files

---
+++ Chap2/src/any.ts
@@ -0,0 +1,5 @@
+let val: any = 22
+val = "string value"
+val = new Array()
+val.push(1)
+console.log(val)
---
+++ Chap2/src/shape.ts
@@ -0,0 +1,9 @@
+class Person {
+  name: string;
+}
+
+const jill: { name: string } = {
+  name: "Jill",
+};
+const person: Person = jill
+console.log(person)
