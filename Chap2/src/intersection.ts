134414b7adb4f5b023f7a8189be0b032eedc6bfe
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2023-02-02 19:14:44 -0600

    add example to intersection.ts

--- Chap2/src/intersection.ts
+++ Chap2/src/intersection.ts
@@ -2,3 +2,11 @@
 let obj: { a: number } & { b: string } = { a: 1, b: "2" }
 
 console.log(obj)
+
+// union
+let union: null | undefined | number = null
+console.log(union)
+union = undefined
+console.log(union)
+union = 1
+console.log(union)
